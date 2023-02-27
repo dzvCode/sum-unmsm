95b41ddc6016f61374165351e3d3d56fa5e3a0bd
PhoenixBlue02 <jean.cardenas2@unmsm.edu.pe>
2023-02-27 15:44:36 -0500

    Update main.py

--- main.py
+++ main.py
@@ -2,12 +2,9 @@ from fastapi import FastAPI
 from database.connection import get_db_connection
 from routes.base_router import api_router
 
-app = FastAPI(title="SUM chiquito")
-
+app = FastAPI(title="SUM Chiquito", description="La API backend realizada con FAST API del gestor de estudiantes y matrícula SUM Chiquito es un sistema que permite a los usuarios gestionar información relacionada con la matrícula y el seguimiento de estudiantes de la Universidad Nacional Mayor de San Marcos.")
 # # Crear conexión a la base de datos
 # conn = get_db_connection()
 
 # cursor = conn.cursor()
-
-
 app.include_router(api_router)
