7f6ef00074fba7fedd713afe2f98cc50c1695dc1
PhoenixBlue02 <jean.cardenas2@unmsm.edu.pe>
2023-02-26 23:46:39 -0500

    Editando el router y schema de Student

--- routes/student_router.py
+++ routes/student_router.py
@@ -1,7 +1,7 @@
 from fastapi import APIRouter
 from database.connection import get_db_connection
 
-from schemas.student import Student
+from schemas.student import CreateStudent, Student, UpdateStudent
 
 
 router = APIRouter()
@@ -12,9 +12,9 @@ cursor = conn.cursor()
 
 # Create student
 @router.post("/")
-async def create_student(student: Student):
-    cursor.execute("INSERT INTO estudiantes (id, nombre, apellido) VALUES (:1, :2, :3)",
-                   (student.id,student.name, student.last_name))
+async def create_student(student: CreateStudent):
+    cursor.execute("INSERT INTO estudiantes (id_estudiante, nombre_completo, correo, telefono, id_escuela) VALUES (:1, :2, :3, :4, :5)",
+                   (student.id,student.name, student.email, student.phone, student.id_school))
     conn.commit()
     return {"message": "Student created successfully"}
 
@@ -22,36 +22,58 @@ async def create_student(student: Student):
 # Read all students
 @router.get("/")
 async def read_students():
-    cursor.execute("SELECT id,nombre, apellido FROM estudiantes")
+    cursor.execute("SELECT id_escuela, nombre_completo, correo FROM estudiantes")
     results = cursor.fetchall()
     students = []
     for result in results:
-        student = Student(id=result[0], name=result[1], last_name=result[2])
+        student = Student(id=result[0], name=result[1], email=result[2])
         students.append(student)
     return students
 
 # Read student by id
 @router.get("/{student_id}")
 async def read_student(student_id: int):
-    cursor.execute("SELECT id,nombre, apellido FROM estudiantes WHERE id=:1", (student_id,))
+    cursor.execute("SELECT id_estudiante,nombre_completo, correo FROM estudiantes WHERE id_estudiante=:1", (student_id,))
     result = cursor.fetchone()
     if result:
-        student = Student(id=result[0], name=result[1], last_name=result[2])
+        student = Student(id=result[0], name=result[1], email=result[2])
         return student
     else:
         return {"message": "Student not found"}
 
 # Update student
 @router.put("/{student_id}")
-async def update_student(student_id: int, student: Student):
-    cursor.execute("UPDATE estudiantes SET nombre=:1, apellido=:2 WHERE id=:3",
-                   (student.name, student.last_name, student_id))
+async def update_student(student_id: int, student: UpdateStudent):
+    # Consultar si existe el estudiante con el ID proporcionado
+    cursor.execute("SELECT id_estudiante FROM estudiantes WHERE id_estudiante=:1", (student_id,))
+    result = cursor.fetchone()
+
+    # Si no se encuentra el estudiante, retornar un mensaje de error
+    if not result:
+        return {"error": "Student not found"}
+
+    # Si el estudiante existe, actualizar sus datos en la base de datos
+    cursor.execute("UPDATE estudiantes SET nombre_completo=:1, correo=:2, telefono=:3 WHERE id_estudiante=:4",
+                   (student.name, student.email, student.phone, student_id))
     conn.commit()
+
     return {"message": "Student updated successfully"}
+
 
 # Delete student
 @router.delete("/{student_id}")
 async def delete_student(student_id: int):
-    cursor.execute("DELETE FROM estudiantes WHERE id=:1", (student_id,))
+    # Consultar si existe el estudiante con el ID proporcionado
+    cursor.execute("SELECT id_estudiante FROM estudiantes WHERE id_estudiante=:1", (student_id,))
+    result = cursor.fetchone()
+
+    # Si no se encuentra el estudiante, retornar un mensaje de error
+    if not result:
+        return {"error": "Student not found"}
+
+    # Si el estudiante existe, eliminarlo de la base de datos
+    cursor.execute("DELETE FROM estudiantes WHERE id_estudiante=:1", (student_id,))
     conn.commit()
+
     return {"message": "Student deleted successfully"}
+
