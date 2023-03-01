1c1abb934fe9d5132f7997134036d8bf8b933737
nick00las <nicolasalonso.rojas@unmsm.edu.pe>
2023-02-28 23:01:25 -0500

    Se modifica el schema según las modificaciones en la db

--- schemas/courses_teachers.py
+++ schemas/courses_teachers.py
@@ -6,14 +6,11 @@ class CT(BaseModel):
     id_teacher: int
     score: float
     date: datetime.date
-    
 class CT_Update(BaseModel):
     id_course: int
     id_teacher: int
-    score: float
-    
+  
 class CT_Create(BaseModel):
     id_course: int
     id_teacher: int
-    score: float
-    date: datetime.date
+
